2587d7287964fbf7f5bb1b4d3d001d054608bf57
Hxan <1051787853@qq.com>
2022-06-13 15:16:02 +0800

    增加历史搜索记录功能

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # from crypt import methods
-from flask import Flask,render_template,redirect,url_for, jsonify
+from flask import Flask, render_template, redirect, url_for, jsonify
 import pymysql
 from model.forms import SearchForm
 from flask import request
@@ -7,6 +7,8 @@ import useful_functions
 import spider_modul
 import spider_news
 import utils
+import time
+
 # 这里对数据库内容进行提取
 # spider_modul.run()
 datalist = useful_functions.get_datalist()
@@ -41,6 +43,7 @@ def temp_page():
 def index():
     return render_template("index.html")
 
+
 # 新闻缩略页
 @app.route('/info')
 def news_page():
@@ -48,7 +51,8 @@ def news_page():
     global line_data_humd
     global line_data_temp
     data_info = datainfo
-    return render_template("info.html",news=data_info, line_data_temp=line_data_temp, line_data_humd=line_data_humd)
+    return render_template("info.html", news=data_info, line_data_temp=line_data_temp, line_data_humd=line_data_humd)
+
 
 # 数据
 @app.route('/info1')
@@ -57,77 +61,92 @@ def info_page():
     global line_data_humd
     global line_data_temp
     data_info = datainfo
-    return render_template("info1.html",news=data_info, line_data_temp=line_data_temp, line_data_humd=line_data_humd)
+    return render_template("info1.html", news=data_info, line_data_temp=line_data_temp, line_data_humd=line_data_humd)
+
 
 @app.route('/kepler_map')
 def kepler_map():
     return render_template("kepler_map.html")
 
+
 # 搜索界面
-@app.route('/search',methods=['GET','POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search_page():
-    form = SearchForm()   
-    return render_template('search.html', form=form)
-        
+    form = SearchForm()
+    return render_template('search.html', form=form, str_list=str_list)
+
+
 # 登录页面
 @app.route('/html/new')
 def new():
-        return render_template('/html/new.html')
-    
+    return render_template('/html/new.html')
+
+
 @app.route('/html/news')
 def news():
-        return render_template('/html/news.html')
-    
+    return render_template('/html/news.html')
+
+
 @app.route('/html/old')
 def old():
-        return render_template('/html/old.html')
-    
+    return render_template('/html/old.html')
+
+
 @app.route('/forcast')
 def forcast():
-        return render_template('/forcast.html')   
-    
+    return render_template('/forcast.html')
+
+
 @app.route('/html/forcasts')
 def forcasts():
-        return render_template('/html/forcasts.html')
-    
+    return render_template('/html/forcasts.html')
+
+
 @app.route('/html/welcome')
 def welcome():
-        return render_template('/html/welcome.html')
-    
+    return render_template('/html/welcome.html')
+
+
 @app.route('/backstage')
 def backstage():
-        return render_template('backstage.html')
-    
+    return render_template('backstage.html')
+
+
 @app.route('/user')
 def user():
-        return render_template('user.html')
-    
+    return render_template('user.html')
+
+
 @app.route('/html/dashboard')
 def dashboard():
-        return render_template('/html/dashboard.html')
-    
+    return render_template('/html/dashboard.html')
+
+
 @app.route('/new_a')
 def new_a():
-        return render_template('/new_a.html')
-    
+    return render_template('/new_a.html')
+
+
 @app.route('/old_a')
 def old_a():
-        return render_template('/old_a.html')
-    
+    return render_template('/old_a.html')
+
+
 @app.route('/html/profile')
 def profile():
-        return render_template('/html/profile.html')
+    return render_template('/html/profile.html')
 
-@app.route('/login',methods=['POST','GET'])
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    if(request.method=='GET'):
+    if (request.method == 'GET'):
         return render_template('login.html')
-    elif(request.method=='POST'):
+    elif (request.method == 'POST'):
         print(request.form.get('username'))
         # time.sleep(1000000)
         return redirect(url_for('backstage'))
-    
-    
+
+
 # 获取分页历史统计数据
 @app.route('/old/list', methods=["POST"])
 def old_list():
@@ -154,6 +173,7 @@ def old_edit():
     WIN_S_Inst_Max = get_data.get('WIN_S_Inst_Max')
     utils.edit_old(city, TEM, TEM_MAX, TEM_MIN, RHU, VAP, PRE_1h, WIN_S_Max, WIN_S_Inst_Max)
     return '200'
+
 
 # 获取分页区域统计数据
 @app.route('/new/list', methods=["POST"])
@@ -186,16 +206,24 @@ def new_edit():
 
 
 # 搜索结果返回界面，返回时展示页数（每页50条数据），（id、新闻文本、新闻url）
-@app.route('/news_result',methods=['POST','GET'])
+str_list = []
+
+
+@app.route('/news_result', methods=['POST', 'GET'])
 def newsResult_page():
     result = []
     newsList = spider_news.spider_news()
-    keyword  = request.form.get('query')
+    keyword = request.form.get('query')
 
-    list_title,list_url = newsList.getDC()
+    # add by lws 历史功能
+    if keyword not in str_list:
+        str_list.append(keyword)
+    # end add
+
+    list_title, list_url = newsList.getDC()
     for i in range(len(list_title)):
         if list_title[i].__contains__(keyword):
-            result.append((i+1, list_title[i], 'http://data.cma.cn' + list_url[i]))
+            result.append((i + 1, list_title[i], 'http://data.cma.cn' + list_url[i]))
     return render_template('news_result.html', form=result)
 
 
---
+++ kelper.py
@@ -0,0 +1,12 @@
+# author:l
+# contact: test@test.com
+# datetime:2022/6/13 14:13
+# software: PyCharm
+# file    : kelper.py     
+# description :
+
+
+import pandas as pd
+from keplergl import KeplerGl
+df = pd.read_csv('datong.csv')
+df.head()
